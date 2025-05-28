284a5f44488c5de8296da75db0248b804fadb303
Harits77 <hariharants77@gmail.com>
2025-05-28 23:48:44 +0530

    first commit

---
+++ src/lib/constants.tsx
@@ -0,0 +1,47 @@
+import project1 from "../assets/Project1.png"
+import project2 from "../assets/Project2.png"
+import project3 from "../assets/Project3.png"
+import project4 from "../assets/Project4.png"
+
+
+export const PROJECTS = [
+
+  {
+    title: "Shopping Cart",
+    description: "A simple shopping cart built in React that allows users to add and remove products.Includes basic cart management features like item tracking and dynamic updates.",
+    image: project2,
+    technologies: ["React.js", "Tailwind.css"],
+    livedemo: "https://shopcart-woad.vercel.app/",
+    code: "https://github.com/Harits77/shopcart-react.git",
+  },
+  {
+    title: "QR Code Generator",
+    description: " A React.js QR code generator with state management, styled using Tailwind CSS, dynamically creates scannable codes from user inputs.",
+    image: project3,
+    technologies: ["React.js", "Tailwind.css"],
+    livedemo: "https://qr-code-generator-two-blue.vercel.app/",
+    code: "https://github.com/Harits77/QR-Code-Generator.git",
+  },
+  {
+    title: "HealthLink Pro",
+    description: "A doctor-patient management system that handles user registration, login, and  displays personalized patient details, allowing doctors to manage patient information efficiently.",
+    image: project1,
+    technologies: ["React.js", "Node.js", "Express.js", "MongoDB"],
+    code: "https://github.com/Harits77/Healthlink-Pro-MERN-Project.git",
+  },
+  {
+    title: "Curd Operations",
+    description: "  CRUD operations in MERN manage data using Node.js, Express.js, and MongoDB for backend processing, whileReact handles the frontend interactions via API calls.",
+    image: project4,
+    technologies: ["React.js", "Node.js", "Express.js", "MongoDB"],
+    code: "https://github.com/Harits77/CURD-MERN.git",
+  },
+  {
+    title: "Login Authentication System",
+    description: "Built a secure login system using the MERN stack with bcrypt for password hashing and MongoDB for user data storage, ensuring secure authentication.",
+    image: "https://images.unsplash.com/photo-1467232004584-a241de8bcf5d?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&h=400",
+    technologies: ["React.js", "Node.js", "Express.js", "MongoDB"],
+    code: "https://github.com/Harits77/LoginForm-MERN.git",
+  },
+
+];
